4a994b687e53b79bcf6b15027871daf8fae6847a
Zeel-1111 <zeelu1111@gmail.com>
2026-07-04 22:14:03 +0530

    fix: rename DockerFile to Dockerfile

--- FlowDesk_Backend/FlowDesk/FlowDesk.API/Program.cs
+++ FlowDesk_Backend/FlowDesk/FlowDesk.API/Program.cs
@@ -169,12 +169,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
 
 
 app.UseIpRateLimiting();
 app.UseCors("AllowFrontend");
-app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();

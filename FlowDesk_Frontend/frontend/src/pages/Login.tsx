1d932a47ac1e0b090c5ba9f741d811f229524973
Zeel-1111 <zeelu1111@gmail.com>
2026-07-18 14:56:33 +0530

    feat: switch to Brevo HTTP API for email sending

--- FlowDesk_Frontend/frontend/src/pages/Login.tsx
+++ FlowDesk_Frontend/frontend/src/pages/Login.tsx
@@ -119,12 +119,7 @@ export default function Login() {
                   </Link>
                 </div>
               </div>
-                
-              <div className="flex justify-end">
-                <Link to="/forgot-password" className="text-xs text-blue-600 hover:underline">
-                  Forgot password?
-                </Link>
-              </div>  
+
 
               <button
                 type="submit"
